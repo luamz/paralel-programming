6f077ce670e72ac2937dcacd350a2d0fa7b898bf
Luam Souza <luamsilva@id.uff.br>
2022-09-21 08:12:05 -0300

    Hello world openMP

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<omp.h>
+
+
+int main (){
+    #pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        printf("hello (%d) ", ID);
+        printf("world (%d)\n", ID);
+    }
+}
+
+// export OMP_NUM_THREADS=2 (bash)
